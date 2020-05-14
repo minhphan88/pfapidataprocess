3603f5876b5182d27a068b2cf5d05075a4a4b497
minhphan88 <minh.phan88@outlook.com>
2020-05-14 16:47:22 +0100

    remove stopword

--- mapping_ultility_functions.py
+++ mapping_ultility_functions.py
@@ -1,11 +1,12 @@
 import nltk
 from nltk.tokenize import RegexpTokenizer
 from nltk.stem import WordNetLemmatizer,PorterStemmer
-from nltk.corpus import stopwords
+#from nltk.corpus import stopwords
 import re
 from difflib import *
 lemmatizer = WordNetLemmatizer()
 stemmer = PorterStemmer()
+stopword_List=['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
 
 def preprocess(sentence):
     sentence=str(sentence)
@@ -17,7 +18,7 @@ def preprocess(sentence):
     rem_num = re.sub('[0-9]+', '', rem_url)
     tokenizer = RegexpTokenizer(r'\w+')
     tokens = tokenizer.tokenize(rem_num)
-    filtered_words = [w for w in tokens if len(w) > 2 if not w in stopwords.words('english')]
+    filtered_words = [w for w in tokens if len(w) > 2 if not w in stopword_List]
     stem_words=[stemmer.stem(w) for w in filtered_words]
     lemma_words=[lemmatizer.lemmatize(w) for w in stem_words]
     return filtered_words
